dba4ee45eed767f8f550cb6e1b8f215439aad59b
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2018-01-11 11:52:41 -0600

    Use Casting when Java automatically places a different data type within your variable. i.e. holding a number in a byte as an integer from a previous variable that states it as a byte

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,19 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! Hola, Mundo");
+        int myFirstNumber = 4;
+        System.out.println(myFirstNumber);
+
+        byte cheeto = 2;
+        double soda = 1.99;
+
+        double snackCost = cheeto + soda;
+        System.out.println(snackCost);
+
+        byte potato = -128;
+        byte mashedPotato = (byte) (potato/2);
+        System.out.println(mashedPotato);
+
     }
 }
 
